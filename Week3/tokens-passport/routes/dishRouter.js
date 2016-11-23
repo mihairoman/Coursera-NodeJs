6a6982cb4522366680565fcc5b3650b248000aa1
mihairoman <mihairoman.r@gmail.com>
2016-11-23 17:00:15 +0100

    Nodejs tokens, passport, passport-mongoose exercise

---
+++ Week3/tokens-passport/routes/dishRouter.js
@@ -0,0 +1,139 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var Dishes = require('../models/dishes')(mongoose),
+    Verify = require('./verify'),
+    app = express();
+
+var dishRouter = express.Router();
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+    .get(Verify.verifyOrdinaryUser, function(req, res, next) {
+
+        Dishes.find({}, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        });
+
+    })
+    .post(Verify.verifyOrdinaryUser, function(req, res, next) {
+
+        Dishes.create(req.body, function(err, dish) {
+            //req.body contains the json formatted information (dish) we want to add to the DB
+            console.log('Dish created');
+            var id = dish._id;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Added dish with id: ' + id);
+        });
+
+    }).delete(Verify.verifyOrdinaryUser, function(req, res, next) {
+
+        Dishes.remove({}, function(err, resp) {
+            if (error) throw err;
+            res.json(resp);
+        });
+
+    });
+
+dishRouter.route('/:dishId')
+    .get(function(req, res, next) {
+
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        })
+
+    })
+    .put(function(req, res, next) {
+
+        Dishes.findByIdAndUpdate(req.params.dishId, {
+            $set: req.body
+        }, {
+            new: true
+        }, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        });
+
+    })
+    .delete(function(req, res, next) {
+
+        Dishes.findByIdAndRemove(req.params.dishId, function(err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+
+    });
+
+dishRouter.route('/:dishId/comments')
+    .get(function(req, res, next) {
+
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            if (err) throw err;
+            res.json(dish.comments);
+        });
+
+    })
+    .post(function(req, res, next) {
+        if (err) throw err;
+
+        dish.comments.push(req.body);
+        dish.save(function(err, dish) {
+            if (err) throw err;
+            console.log('Updated Comments for ', dish);
+            res.json(dish);
+        });
+    })
+    .delete(function(req, res, next) {
+
+        Dishes.findById(req.patams.dishId, function(err, dish) {
+            if (err) throw err;
+            var i = dish.comments.length - 1;
+            for (i; i >= 0; i--) {
+                dish.comments.id(dish.comments[i]._id).remove();
+            }
+            dish.save(function(err, result) {
+                if (err) throw err;
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end('All comments were deleted!');
+            })
+        });
+
+    });
+
+dishRouter.route('/:dishId/comments/:commentId')
+    .get(function(req, res, next) {
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            if (err) throw err;
+            res.json(dish.comments.id(req.params.commentId));
+        });
+    })
+    .put(function(req, res, next) {
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            if (err) throw err;
+            dish.comments.id(req.params.commentId).remove();
+            dish.comments.push(req.body);
+            dish.save(function(err, dish) {
+                if (err) throw err;
+                console.log('Updated Comments for : ', dish);
+                res.json(dish);
+            });
+        });
+    })
+    .delete(function(req, res, next) {
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            dish.comments.id(req.params.commentId).remove();
+            dish.save(function(err, resp) {
+                if (err) throw err;
+                res.json(resp);
+            })
+        });
+    });
+
+module.exports = dishRouter;
---
+++ Week3/tokens-passport/routes/leaderRouter.js
@@ -0,0 +1,73 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var Leaders = require('../models/leaders')(mongoose);
+var app = express();
+
+var leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+
+var leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+    .get(function(req, res, next) {
+
+        Leaders.find({}, function(err, leader) {
+            if (err) throw err;
+            res.json(leader);
+        });
+
+    })
+    .post(function(req, res, next) {
+
+        Leaders.create(req.body, function(err, leader) {
+            //req.body contains the json formatted information (leader) we want to add to the DB
+            console.log('Leader created');
+            var id = leader._id;
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('Added leader with id: ' + id);
+        });
+
+    }).delete(function(req, res, next) {
+
+        Leaders.remove({}, function(err, resp) {
+            if (error) throw err;
+            res.json(resp);
+        });
+
+    });
+
+leaderRouter.route('/:leaderId')
+    .get(function(req, res, next) {
+
+        Leaders.findById(req.params.leaderId, function(err, leader) {
+            if (err) throw err;
+            res.json(leader);
+        })
+
+    })
+    .put(function(req, res, next) {
+
+        Leaders.findByIdAndUpdate(req.params.leaderId, {
+            $set: req.body
+        }, {
+            new: true
+        }, function(err, leader) {
+            if (err) throw err;
+            res.json(leader);
+        });
+
+    })
+    .delete(function(req, res, next) {
+
+        Leaders.findByIdAndRemove(req.params.leaderId, function(err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+
+    });
+module.exports = leaderRouter;
